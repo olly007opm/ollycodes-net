f55d15569ff291021f4ed08093064c34e2fb8af0
Olly <olly@ollycodes.net>
2024-06-26 22:21:09 +0100

    Add clock

--- src/components/Taskbar.svelte
+++ src/components/Taskbar.svelte
@@ -1,3 +1,24 @@
+<script lang="ts">
+    import { format } from "date-fns"
+    import { onMount } from "svelte"
+
+    let time = new Date()
+    onMount(() => {
+        const interval = setInterval(() => time = new Date(), 1000)
+        return () => clearInterval(interval)
+    })
+</script>
+
 <div id="task-bar">
-    <button class="btn btn-start">Start</button>
+    <div>
+        <button class="btn btn-start">Start</button>
+        <div class="separator"></div>
+    </div>
+
+    <div>
+        <div class="separator"></div>
+        <div class="time-box">
+            <span>{format(time, "hh:mm:ss a")}</span>
+        </div>
+    </div>
 </div>
