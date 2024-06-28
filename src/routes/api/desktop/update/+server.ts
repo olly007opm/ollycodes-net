b312d9be00b638aadc4a83e44a693d462e742a45
Olly <olly@ollycodes.net>
2024-06-28 21:21:20 +0100

    Add error message when trying to modify another user's desktop

--- src/routes/api/desktop/update/+server.ts
+++ src/routes/api/desktop/update/+server.ts
@@ -6,6 +6,9 @@ export async function POST({ request, locals }) {
     if (!session || !session.user) return error(403, "Unauthorized")
 
     let data: { id: string, x: number, y: number } = await request.json()
+    if (prisma.desktop.findFirst({ where: { id: data.id } }).user !== session.user) {
+        return error(403, "Unauthorized")
+    }
     await prisma.desktopItem.update({
         where: { id: data.id, desktop: { user: session.user } },
         data: { x: data.x, y: data.y }
