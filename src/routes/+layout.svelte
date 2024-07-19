79063631d7d55e7541abd611926519e4c1f61e14
Olly <olly@ollycodes.net>
2024-07-19 16:02:49 +0100

    Add Vercel analytics and speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import "$css/main.css"
+    import { inject } from '@vercel/analytics'
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit'
+    inject()
+    injectSpeedInsights()
 </script>
 
 <slot />
